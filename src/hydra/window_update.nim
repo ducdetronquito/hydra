59d597da984cf026fab134b6b019bf9558614878
ducdetronquito <g.paulet@zero-gachis.com>
2020-02-06 14:50:26 +0100

    :sparkles: Can serialize WindowUpdate frame.

--- src/hydra/window_update.nim
+++ src/hydra/window_update.nim
@@ -23,3 +23,14 @@ proc read*(cls: type[WindowUpdateFrame], header: Header, stream: StringStream): 
         return Err(ErrorCode.Protocol)
 
     return Ok(WindowUpdateFrame(header: header, window_size_increment: window_size_increment))
+
+
+proc serialize*(self: WindowUpdateFrame): seq[byte] =
+    result = self.header.serialize()
+    let window_size_increment = cast[uint32](self.window_size_increment)
+    result.add(cast[uint8](window_size_increment shr 24'u32))
+    result.add(cast[uint8](window_size_increment shr 16'u32))
+    result.add(cast[uint8](window_size_increment shr 8))
+    result.add(cast[uint8](window_size_increment))
+
+    return result
--- tests/test_window_frame.nim
+++ tests/test_window_frame.nim
@@ -21,3 +21,17 @@ suite "Window Update Frame":
         let header = Header(frame_type: FrameType.WindowUpdate, length: 4'u32, stream_id: 1'u32)
         let result = WindowUpdateFrame.read(header, stream)
         check(result.unwrap_error() == ErrorCode.Protocol)
+
+    test "Serialize":
+        let header = Header(
+            length: 4'u32,
+            frame_type: FrameType.WindowUpdate,
+            flags: 0'u8,
+            stream_id: StreamId(1'u32)
+        )
+
+        let frame = WindowUpdateFrame(header: header, window_size_increment: 42'u32)
+        check(frame.serialize() == [
+            0'u8, 0'u8, 4'u8, 8'u8, 0'u8, 0'u8, 0'u8, 0'u8, 1'u8,
+            0'u8, 0'u8, 0'u8, 42'u8,
+        ])
